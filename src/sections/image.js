95634257ae2775194398736a04bf44d8f07e5acb
GastonPicco <117951553+GastonPicco@users.noreply.github.com>
2024-06-28 04:20:08 -0300

    test

--- src/sections/image.js
+++ src/sections/image.js
@@ -1,104 +1,104 @@
-import backgroundImg from './../assets/background.jpg';
-import UnityClogo from './../assets/UC-logo.png';
-import UnityCB from './../assets/UCBlender-logo.png';
+import backgroundImg from '../assets/background.jpg';
+import UnityClogo from '../assets/UC-logo.png';
+import UnityCB from '../assets/UCBlender-logo.png';
 // Iconos
-import blenderICO from './../icons/blender.png';
-import camtasiaICO from './../icons/camtasia.png';
-import capcutICO from './../icons/capcut.png';
-import cinema4dICO from './../icons/cinema4d.png';
-import csharpICO from './../icons/csharp.png';
-import cssICO from './../icons/css.png';
-import gitICO from './../icons/git.png';
-import htmlICO from './../icons/html.png';
-import javaICO from './../icons/java.png';
-import mongoDBICO from './../icons/mongoDB.png';
-import nodeJSICO from './../icons/nodeJS.png';
-import reactICO from './../icons/react.png';
-import unityICO from './../icons/unity.png';
-import visualstudioICO from './../icons/visualstudio.png';
-import vscodeICO from './../icons/vscode.png';
-import jsICO from './../icons/JS.png';
+import blenderICO from '../icons/blender.png';
+import camtasiaICO from '../icons/camtasia.png';
+import capcutICO from '../icons/capcut.png';
+import cinema4dICO from '../icons/cinema4d.png';
+import csharpICO from '../icons/csharp.png';
+import cssICO from '../icons/css.png';
+import gitICO from '../icons/git.png';
+import htmlICO from '../icons/html.png';
+import javaICO from '../icons/java.png';
+import mongoDBICO from '../icons/mongoDB.png';
+import nodeJSICO from '../icons/nodeJS.png';
+import reactICO from '../icons/react.png';
+import unityICO from '../icons/unity.png';
+import visualstudioICO from '../icons/visualstudio.png';
+import vscodeICO from '../icons/vscode.png';
+import jsICO from '../icons/JS.png';
 // pong2d
-import pong2Dmenujpg from './../assets/pong2d/menu.jpg'
-import pong2Dproyectjpg from './../assets/pong2d/proyect.jpg'
-import pong2Dstartgif from './../assets/pong2d/start.gif'
-import pong2Dendgif from './../assets/pong2d/end.gif'
+import pong2Dmenujpg from '../assets/pong2d/menu.jpg'
+import pong2Dproyectjpg from '../assets/pong2d/proyect.jpg'
+import pong2Dstartgif from '../assets/pong2d/start.gif'
+import pong2Dendgif from '../assets/pong2d/end.gif'
 //arkanoid
-import arkanoid2Dgameplayjpg from './../assets/arkanoid/gameplay.jpg'
-import arkanoid2Dmenujpg from './../assets/arkanoid/menu.jpg'
-import arkanoid2Dcontrolsjpg from './../assets/arkanoid/controls.jpg'
-import arkanoid2Dwinjpg from './../assets/arkanoid/win.jpg'
-import arkanoid2Dunityjpg from './../assets/arkanoid/unity.jpg'
-import arkanoid2Dplaygif from './../assets/arkanoid/play.gif'
-import arkanoid2Dcombogif from './../assets/arkanoid/combo.gif'
+import arkanoid2Dgameplayjpg from '../assets/arkanoid/gameplay.jpg'
+import arkanoid2Dmenujpg from '../assets/arkanoid/menu.jpg'
+import arkanoid2Dcontrolsjpg from '../assets/arkanoid/controls.jpg'
+import arkanoid2Dwinjpg from '../assets/arkanoid/win.jpg'
+import arkanoid2Dunityjpg from '../assets/arkanoid/unity.jpg'
+import arkanoid2Dplaygif from '../assets/arkanoid/play.gif'
+import arkanoid2Dcombogif from '../assets/arkanoid/combo.gif'
 //SpaceInvaders
-import spaceInvaders2Dplay2jpg from './../assets/spaceinvaders/play2.jpg'
-import spaceInvaders2Dmenujpg from './../assets/spaceinvaders/menu.jpg'
-import spaceInvaders2Dunityjpg from './../assets/spaceinvaders/unity.jpg'
-import spaceInvaders2Dplay1jpg from './../assets/spaceinvaders/play1.jpg'
-import spaceInvaders2Dbossjpg from './../assets/spaceinvaders/boss.jpg'
-import spaceInvaders2Dbossfightgif from './../assets/spaceinvaders/bossfight.gif'
-import spaceInvaders2Dstartgif from './../assets/spaceinvaders/start.gif'
+import spaceInvaders2Dplay2jpg from '../assets/spaceinvaders/play2.jpg'
+import spaceInvaders2Dmenujpg from '../assets/spaceinvaders/menu.jpg'
+import spaceInvaders2Dunityjpg from '../assets/spaceinvaders/unity.jpg'
+import spaceInvaders2Dplay1jpg from '../assets/spaceinvaders/play1.jpg'
+import spaceInvaders2Dbossjpg from '../assets/spaceinvaders/boss.jpg'
+import spaceInvaders2Dbossfightgif from '../assets/spaceinvaders/bossfight.gif'
+import spaceInvaders2Dstartgif from '../assets/spaceinvaders/start.gif'
 //juegoPlataformas
-import plataformas2Dstartjpg from './../assets/plataformas/start.jpg'
-import plataformas2Dunityjpg from './../assets/plataformas/unity.jpg'
-import plataformas2Dplaygif from './../assets/plataformas/play.gif'
-import plataformas2Dwhatergif from './../assets/plataformas/whater.gif'
-import plataformas2Dendgif from './../assets/plataformas/end.gif'
+import plataformas2Dstartjpg from '../assets/plataformas/start.jpg'
+import plataformas2Dunityjpg from '../assets/plataformas/unity.jpg'
+import plataformas2Dplaygif from '../assets/plataformas/play.gif'
+import plataformas2Dwhatergif from '../assets/plataformas/whater.gif'
+import plataformas2Dendgif from '../assets/plataformas/end.gif'
 //pacman2D
-import pacman2Dmenujpg from './../assets/pacman/menu.jpg'
-import pacman2Dplayjpg from './../assets/pacman/play.jpg'
-import pacman2Dscenejpg from './../assets/pacman/scene.jpg'
-import pacman2Dunityjpg from './../assets/pacman/unity.jpg'
-import pacman2DEatAndLosegif from './../assets/pacman/EatAndLose.gif'
-import pacman2Dgameplaygif from './../assets/pacman/gameplay.gif'
+import pacman2Dmenujpg from '../assets/pacman/menu.jpg'
+import pacman2Dplayjpg from '../assets/pacman/play.jpg'
+import pacman2Dscenejpg from '../assets/pacman/scene.jpg'
+import pacman2Dunityjpg from '../assets/pacman/unity.jpg'
+import pacman2DEatAndLosegif from '../assets/pacman/EatAndLose.gif'
+import pacman2Dgameplaygif from '../assets/pacman/gameplay.gif'
 //pong3d
-import pong3Dpong3djpg from './../assets/pong3d/pong3D.jpg'
-import pong3Dmenujpg from './../assets/pong3d/menu.jpg'
-import pong3Dinspectorjpg from './../assets/pong3d/inspector.jpg'
-import pong3Dgameplaygif from './../assets/pong3d/gameplay.gif'
+import pong3Dpong3djpg from '../assets/pong3d/pong3D.jpg'
+import pong3Dmenujpg from '../assets/pong3d/menu.jpg'
+import pong3Dinspectorjpg from '../assets/pong3d/inspector.jpg'
+import pong3Dgameplaygif from '../assets/pong3d/gameplay.gif'
 //juego movil
-import testMovilplay from './../assets/testmovil/play.jpg'
-import testMovilinspector from './../assets/testmovil/inspector.jpg'
-import testMovilgameplayGif from './../assets/testmovil/gameplay.gif'
+import testMovilplay from '../assets/testmovil/play.jpg'
+import testMovilinspector from '../assets/testmovil/inspector.jpg'
+import testMovilgameplayGif from '../assets/testmovil/gameplay.gif'
 //fruit ninja
-import fruitninjagame from './../assets/fruitninja/game.jpg'
-//import fruitninjamenu2 from './../assets/fruitninja/menu2.jpg'
-import fruitninjainspector from './../assets/fruitninja/inspector.jpg'
-import fruitninjagameplayGIF from './../assets/fruitninja/gameplayninja.gif'
+import fruitninjagame from '../assets/fruitninja/game.jpg'
+import fruitninjamenu2 from '../assets/fruitninja/menu2.jpg'
+import fruitninjainspector from '../assets/fruitninja/inspector.jpg'
+import fruitninjagameplayGIF from '../assets/fruitninja/gameplayninja.gif'
 //bombardero
-import bombarderomenu from './../assets/bombergame/menu.jpg'
-import bombarderoinspector from './../assets/bombergame/inspector.jpg'
-import bombarderoinspector2 from './../assets/bombergame/inspector2.jpg'
-import bombarderoplay from './../assets/bombergame/play.jpg'
-import bombarderolose from './../assets/bombergame/lose.jpg'
-import bombarderogameplaywinGIF from './../assets/bombergame/gameplay_win.gif'
-import bombarderogameplayloseGIF from './../assets/bombergame/gameplay_lose.gif'
+import bombarderomenu from '../assets/bombergame/menu.jpg'
+import bombarderoinspector from '../assets/bombergame/inspector.jpg'
+import bombarderoinspector2 from '../assets/bombergame/inspector2.jpg'
+import bombarderoplay from '../assets/bombergame/play.jpg'
+import bombarderolose from '../assets/bombergame/lose.jpg'
+import bombarderogameplaywinGIF from '../assets/bombergame/gameplay_win.gif'
+import bombarderogameplayloseGIF from '../assets/bombergame/gameplay_lose.gif'
 //survandbuild
-import buildgame from './../assets/buildgame/game.jpg'
-import buildinspector from './../assets/buildgame/inspector.jpg'
-import buildmenu from './../assets/buildgame/menu.jpg'
-import buildoptins from './../assets/buildgame/options.jpg'
-import buildcarrywood from './../assets/buildgame/carrywood.jpg'
-import buildstonemine from './../assets/buildgame/stonemine.jpg'
-import buildslime from './../assets/buildgame/slime.jpg'
-import buildbuilding from './../assets/buildgame/building.jpg'
-import buildmenugif from './../assets/buildgame/menugif.gif'
-import buildminegif from './../assets/buildgame/minegif.gif'
-import buildbuildgif from './../assets/buildgame/buildgif.gif'
+import buildgame from '../assets/buildgame/game.jpg'
+import buildinspector from '../assets/buildgame/inspector.jpg'
+import buildmenu from '../assets/buildgame/menu.jpg'
+import buildoptins from '../assets/buildgame/options.jpg'
+import buildcarrywood from '../assets/buildgame/carrywood.jpg'
+import buildstonemine from '../assets/buildgame/stonemine.jpg'
+import buildslime from '../assets/buildgame/slime.jpg'
+import buildbuilding from '../assets/buildgame/building.jpg'
+import buildmenugif from '../assets/buildgame/menugif.gif'
+import buildminegif from '../assets/buildgame/minegif.gif'
+import buildbuildgif from '../assets/buildgame/buildgif.gif'
 //pool
-import poolpool from './../assets/poolgame/pool.jpg'
-import poolinspector from './../assets/poolgame/inspector.jpg'
-import poolpoolgamegif from './../assets/poolgame/gamegif.gif'
-import poolpausegif from './../assets/poolgame/pausegif.gif'
+import poolpool from '../assets/poolgame/pool.jpg'
+import poolinspector from '../assets/poolgame/inspector.jpg'
+import poolpoolgamegif from '../assets/poolgame/gamegif.gif'
+import poolpausegif from '../assets/poolgame/pausegif.gif'
 //pacman3d
-import pacmangame from './../assets/pacman3/game.jpg'
-import pacmaninspector from './../assets/pacman3/inspector.jpg'
-import pacmanmenu from './../assets/pacman3/menu.jpg'
-import pacmanmenugif from './../assets/pacman3/menugif.gif'
-import pacmanlevel1gif from './../assets/pacman3/level1gif.gif'
-import pacmanlevel2gif from './../assets/pacman3/level2gif.gif'
-import pacmanlevel3gif from './../assets/pacman3/level3gif.gif'
+import pacmangame from '../assets/pacman3/game.jpg'
+import pacmaninspector from '../assets/pacman3/inspector.jpg'
+import pacmanmenu from '../assets/pacman3/menu.jpg'
+import pacmanmenugif from '../assets/pacman3/menugif.gif'
+import pacmanlevel1gif from '../assets/pacman3/level1gif.gif'
+import pacmanlevel2gif from '../assets/pacman3/level2gif.gif'
+import pacmanlevel3gif from '../assets/pacman3/level3gif.gif'
 
 
 const Images = {
@@ -167,7 +167,7 @@ const Images = {
     testMovilgameplayGif,
     //fruit ninja
     fruitninjagame,
-    //fruitninjamenu2,
+    fruitninjamenu2,
     fruitninjainspector,
     fruitninjagameplayGIF,
     //bombardero
